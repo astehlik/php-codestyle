e347c8fac1ec15c54a6e51af1f64c1fa3646d93b
Alexander Stehlik <alexander.stehlik@gmail.com>
2022-02-07 20:29:11 +0100

    [TASK] Add PSR12 rules for php-cs-fixer

---
+++ PhpCsFixer/Psr12DefaultRules.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    '@PhpCsFixer' => true,
+    '@PhpCsFixer:risky' => true,
+    '@PSR1' => true,
+    '@PSR2' => true,
+    '@PSR12' => true,
+    '@PHP71Migration:risky' => true,
+    'concat_space' => ['spacing' => 'one'],
+    'yoda_style' => [
+        'equal' => false,
+        'identical' => false,
+        'less_and_greater' => null,
+    ],
+    'cast_spaces' => ['space' => 'none'],
+    'native_function_invocation' => ['include' => []],
+    'phpdoc_align' => ['align' => 'left'],
+    'phpdoc_types_order' => ['null_adjustment' => 'always_last'],
+    'ordered_class_elements' => [
+        'sort_algorithm' => 'alpha',
+        'order' => [
+            'use_trait',
+            'constant_public',
+            'constant_protected',
+            'constant_private',
+            'property_public_static',
+            'property_protected_static',
+            'property_private_static',
+            'property_public',
+            'property_protected',
+            'property_private',
+            'method_public_abstract_static',
+            'method_protected_abstract_static',
+            'method_public_abstract',
+            'method_protected_abstract',
+            'construct',
+            'destruct',
+            'magic',
+            'phpunit',
+            'method_public_static',
+            'method_protected_static',
+            'method_private_static',
+            'method_public',
+            'method_protected',
+            'method_private',
+        ],
+    ],
+];
